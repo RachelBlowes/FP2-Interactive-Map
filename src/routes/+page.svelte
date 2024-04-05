aaf8cadf77270c2c7c1fb20d6c8b169a3becaab0
RachelBlowes <rachelmb@mit.edu>
2024-04-05 17:06:36 -0400

    add data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	<title>Interactive Map</title>
 </svelte:head>
 
+<h1>Interactive Map</h1>
+
+<div id="map" />
 
 <script>
 import mapboxgl from "mapbox-gl";
@@ -10,21 +13,40 @@ mapboxgl.accessToken = "pk.eyJ1IjoicmFjaGVsbWIiLCJhIjoiY2x1bjFtbDUwMHN3YTJrb2Eya
 
 import { onMount } from "svelte";
 
-onMount(() => {
-	let map = new mapboxgl.Map({
-		container: 'map';
-		style: 'mapbox://styles/mapbox/streets-v12',
-		center: [-71.0589, 42.3601], // starting position [lng, lat]
-        zoom: 11
-	});
-})
+  async function loadMap() {
+    return new Promise(resolve => {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/rachelmb/cluisi2c0003o01p2c4mdf1k2',
+        center: [-71.0589, 42.3601],
+        zoom: 12
+      });
 
+      map.on('load', () => resolve(map));
+    });
+  }
 
+  onMount(async () => {
+    const map = await loadMap();
+
+    // Once the map is loaded, add the source
+    map.addSource("boston_parcels", {
+      type: "geojson",
+      data: "C:\Users\rache\OneDrive\Documents\GitHub\FP2-Interactive-Map\geodata\Boston_sales_since_2010.geojson",
+    });
+
+    // After adding the source, add the layer
+    map.addLayer({
+      id: "boston_parcels", // A unique name for the layer
+      type: "fill", // The layer type, in this case, it's a line layer
+      source: "boston_parcels", // The id of the source we added earlier
+      paint: { "fill-color": "blue", // Fill color for the polygons
+        "fill-opacity": 0.5 // Opacity of the fill
+      },
+    });
+  });
 
 
 </script>
 
 
-<h1>Interactive Map</h1>
-
-<div id="map" />
