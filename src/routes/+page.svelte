5d8f201febf11a198e0173f915ec7e606bd93cba
RachelBlowes <rachelmb@mit.edu>
2024-04-25 14:32:34 -0400

    edit scrollytelling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,7 @@
   <script src="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js"></script>
 </svelte:head>
 
-<div id="cover">
-  <h1>Renovation | Speculation</h1>
-  <img id="coverphoto" src="src/lib/coverphoto.png" alt=""> 
-</div>
-
-<Scroller top="{0.2}" bottom="{0.8}" bind:index bind:offset bind:progress>
+<Scroller top="{0.0}" bottom="{0.7}" bind:index bind:offset bind:progress>
   <div slot="background">
     
     <img id="fadedcoverphoto" src="src/lib/coverphoto.png" alt=""> 
@@ -20,17 +15,38 @@
 
   <div slot="foreground">
     <section> 
-      <!-- This is the first section -->
+      <!-- This is the 1st section -->
+      <div id="cover">
+        <h1>Renovation | Speculation</h1>
+        <img id="coverphoto" src="src/lib/coverphoto.png" alt=""> 
+      </div>
+    </section>
+    <section> 
+      <!-- This is the 2nd section -->
       <h2> Mapping speculative investment in Boston’s housing market using data on renovation, construction, and sales </h2>
     </section>
     <section>
-      <!-- This is the second section. -->
+      <!-- This is the 3rd section. -->
       <h3>Between 2010 and 2022, housing prices in Boston have increased by xx%</h3>
     </section>
     <section>
-      <!-- This is the third section. -->
-      <p>We use public building permit data from 2010-2023 in addition to sales and assessment data to better understand the relationship between fiscal and physical investment in the housing market.</p>
-     
+      <!-- This is the 4th section. -->
+      <p>We use public building permit data from 2010-2023 in addition to sales and assessment data to better understand the relationship between <strong>physical</strong> and <strong>fiscal</strong> investment in the housing market.</p>
+    </section>
+    <section>
+      <!-- This is the 5th section. -->
+      <div id="definitions">
+        <div id="physical">
+          <p id="physicaltitle">Physical (adj)</p>
+          <p><em>tangible or concrete</em></p>
+          <p>In our analysis, physical investments refer to money spent on physical improvements to a given property</p>
+        </div>
+        <div id="fiscal">
+          <p id="fiscaltitle">Fiscal (adj)</p>
+          <p><em>relating to financial matters</em></p>
+          <p>In our analysis, fiscal investments refer to residential real estate transactions</p>
+        </div>     
+      </div>
     </section>
   </div>
 </Scroller>
@@ -89,26 +105,49 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
-   
+    
 }
 
 #cover h1 {
-  margin-bottom: 0.5rem; /* Adjust as needed */
+  font-size: x-large;
   position: absolute;
 
 }
 
-
-
   #coverphoto {
     max-width: 100%;
-    
+       
   }
 
   #fadedcoverphoto {
     max-width: 100%;
     opacity: 0.5;
+  
+  }
+
+  #definitions {
+    display: flex;
+    align-content: space-around;
+    max-width: 100%;
+    margin: 50px;
 
   }
+
+  #physical, #fiscal {
+    flex: 1; 
+    max-width: 50%; 
+    padding: 0 90px; 
+    
+  }
+
+  #physicaltitle {
+    color: #0087EC;
+    font-weight: bold;
+  }
+  #fiscaltitle {
+    color: #EE6553;
+    font-weight: bold;
+  }
+
 </style>
 
