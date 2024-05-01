3955701773196f25cce136caaaf4fad91db73e9e
Adi <107775334+Kup135@users.noreply.github.com>
2024-05-01 09:30:58 -0400

    example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -140,14 +140,14 @@ building foreshadowing patterns of buying. </h3>
   <p>Move the slider to see changes in owner over time.</p>
   <div style="height: 80px;"></div>
 
-  <!-- <h3> Corporate investors are investing heavily in the construction and renovation of single-family units, 
+  <h3> Corporate investors are investing heavily in the construction and renovation of single-family units, 
       which are more profitable. </h3>
 
   <div style="height: 40px;"></div>
   
   <h3> Properties owned by corporate owners are more likely to be flipped. </h3>
 
-  <div style="height: 40px;"></div> -->
+  <div style="height: 40px;"></div>
 
 <!-- WHERE? ------------------------------------------------------------------->
 
@@ -190,16 +190,95 @@ whereas building permits are more infrequent for the lowest-income and highest-i
   <div id="divline"></div>
 </div>
 
+<Scroller top="{0.0}" bottom="{0.7}" bind:index bind:offset bind:progress>
+  <div slot="background">
+    
+    <img id="fadedcoverphoto" src="https://rachelblowes.github.io/Geodata/Images/coverphoto.png" alt="Split picture showing an apartment before and after renovation"> 
+    
+  </div>
+
+  <div slot="foreground">
+    <sectio>
+      <br>
+    </sectio>
+
+    <section>
+      <!-- Example - intro -->
+      <p id="centered-text"><strong>Let's explore a speculated property.</strong></p>
+      <p id="centered-text"><strong>How much profit do you think the investor made?</strong></p>
+    </section>
+
+    <section>
+      <!-- Example 01-->
+      <div id="definitions" class="collage">
+        <div id="physical">
+          <h1 id="example_header" style="color: black; text-align: left;">11 Joseph St</h1>
+          <h5 id="example_typology_header" style="text-align: left">2 Family Residential House</h5>
+          <p id="paragraph_sale1">An investor bought 11 Joseph st. in July 2015 for <strong id="fiscaltitle">$238,000.</strong></p>
+          <br>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/11Joseph_2014.png?raw=true" alt="11 Joseph St 2014" class="GSV">
+          <p><em>Google Street View, 2014</em></p>
+        </div>
+        <div id="fiscal">
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/1st.png?raw=true" alt="Ground Floor plan" class="arch_plan"  style="margin-top: 5em">
+          <p >They then immediately started off with an investment of <strong id="physicaltitle">$12,975</strong> (fees included) installing vinyl sidings on the entire exterior, repaired the front porch, and re-issued a gaz permit.</p>
+        </div>  
+      </div>
+    </section>
+    <section>
+      <!-- Example 02 - sale + 2nd renovation -->
+      <div id="definitions" class="collage">
+        <div id="physical" style="margin-top: -3em">
+          <p id="paragraph_sale1" style="margin-top: 10em">A year from purchasing, he sold the renovated first floor in August 2015 as a standalone dwelling unit for <strong id="fiscaltitle">$425,000.</strong></p>
+          <br>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/11Joseph_2019.png?raw=true" alt="11 Joseph St 2019" class="GSV">
+          <p><em>Google Street View, 2019</em></p>
+        </div>
+        <div id="fiscal" style="margin-top:40em">
+          <p style="position: relative; top: -14.5em;">In the next step they invested <strong id="physicaltitle">$17,700</strong> in a Face-Lift for the second and third floors: retiling the 2 bathrooms on both floors, replacing kitchen cabinets, replacing interior doors, and painting.</p>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/3rd.png?raw=true" alt="Second Floor plan" class="arch_plan"  style="margin-top: -29em">
+        </div>  
+      </div>
+    </section>
+    <section>
+      <!-- Example 03 - 2 last sales -->
+      <div id="definitions" class="collage">
+        <div id="physical" style="margin-top: -3em">
+          <p id="paragraph_sale1" style="margin-top: 10em">The investor sold the apartment on the second floor on March 2017 for <strong id="fiscaltitle">$727,000.</strong>, and the third floor as a studio flat for <strong id="fiscaltitle">$144,641</strong> on Dec 2018. 
+            The last sale also converted the property from a two-family residential housing to <strong id="physicaltitle">a 3 family residential housing.</p>
+          <br>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/reno_img/2017details_kitchen2.jpeg?raw=true" width=400px>
+          <p><em>REDFIN Real Estate</em></p>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/reno_img/2017details.jpeg?raw=true" style="position: relative; top: 2em; left:0em" width=400px>
+        </div>
+        <div id="fiscal" style="margin-top:40em">
+          <p style="position: relative; top: -14.5em; left:-2em;">Between the 2nd and the 3rd sales, they also invested <strong id="physicaltitle">$7,187</strong> in upgrading the electricity infrastructure and adding 1 public meter, as well as replacing 3 windows. </p>
+          <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/11_JosephSt/reno_img/2017details_bathroom.jpeg?raw=true" style="position: relative; top: -13em; left:-8em" width=500px>
+        </div>  
+      </div>
+    </section>
+    <section>
+      <h3 style="position: relative; top: 30em;"> In less than 4 years, and an initial investment of <b>$250,975</b>, the investor made a profit of</h3> 
+      <!-- <br> -->
+      <h1 style="position: relative; top: 30em;"><strong id="fiscaltitle">Over $1MM</strong></h1>
+    </section>
+
+    <section>
+      <br>
+    </section>
+</Scroller>
+
+<div style="text-align: center;">
+  <img src="https://github.com/RachelBlowes/FP2-Interactive-Map/blob/main/src/lib/raster_graphs/Flipped-01.jpg?raw=true" alt="line graph" width="600" height="600">
+</div>
+
 <div style="height: 20px;"></div>
 
-<h3> We found that physical investment always results in a higher sales price difference for residential properties. Since 2010, 
-  this price difference has increased from $16,000 to nearly $70,000, outpacing inflation and demonstratrating a decoupling of investment and value. </h3>
+<h3> We found that physical investment did not have a major impact on the sale price of flipped parcels, although parcels 
+    we classified as renovated were always slightly more expensive. In 2020, flipped parcels were less likely to have building 
+    permits, indicating a decoupling of fiscal and physical investment.  </h3>
 
 <div style="height: 80px;"></div>
-
-<h5> Median profit (Sales profit - building permit valuation - permit fees) from 2010 - 2022. </h5>
-
-<PermitSalesDiffLineGraph />
 
 <script>
 
@@ -237,9 +316,6 @@ whereas building permits are more infrequent for the lowest-income and highest-i
 
     //DotAnimation
     import DotAnimation from '$lib/DotAnimation.svelte';
-
-    //permit sales line graph
-    import PermitSalesDiffLineGraph from '$lib/PermitSalesDiffLineGraph.svelte';
 
 
 </script>
