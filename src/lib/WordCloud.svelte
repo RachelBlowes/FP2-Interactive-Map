916066f1cc8cbe623bceee0394a81ed6d2b81cf4
Adi <107775334+Kup135@users.noreply.github.com>
2024-05-08 00:36:41 -0400

    cloud

--- src/lib/WordCloud.svelte
+++ src/lib/WordCloud.svelte
@@ -1,7 +1,8 @@
 <script>
 	import Flipped from './FlippedCloud.svelte'
 	import NonSold from './NonSoldCloud.svelte'
-	
+    import { fade } from 'svelte/transition';
+    let isVisible = true;
     let current = 'NonSold';
 	let selected = NonSold
     
@@ -12,17 +13,33 @@
 
 </script>
 
-<svelte:component this={selected} />
-<h5>Show what is renovated in:</h5>
-<button  class:active={current === 'NonSold'} on:click={() => selectShape('NonSold', NonSold)}>
-	Owned 
-</button>
-<button class:active={current === 'Flipped'} on:click={() => selectShape('Flipped', Flipped)}>
-	Flipped
-</button>
-
-
+<div class="container">
+<div  in:fade={{ duration: 1000 }}>
+    <svelte:component this={selected}/>
+</div>
+    <h5>Show what is renovated in:</h5>
+    <div>
+        <button  class:active={current === 'NonSold'} on:click={() => selectShape('NonSold', NonSold)}>
+            Owned 
+        </button>
+        <button class:active={current === 'Flipped'} on:click={() => selectShape('Flipped', Flipped)}>
+            Flipped
+        </button>
+    </div>
+</div>
 
 <style>
-
+    div {
+        display: flex;
+        justify-content: center;
+        margin-top: 10px;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10em;
+    }
 </style>
+
+
