8fd4eaddd336ff34e06e8d6bcf7ed8366b8b4f27
RachelBlowes <rachelmb@mit.edu>
2024-05-09 17:46:32 -0400

    syn zoom pan

--- src/lib/TimeSliderMaps.svelte
+++ src/lib/TimeSliderMaps.svelte
@@ -227,122 +227,146 @@
 
 
 <script>
-import mapboxgl from "mapbox-gl";
-import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
-mapboxgl.accessToken = "pk.eyJ1IjoicmFjaGVsbWIiLCJhIjoiY2x1bjFtbDUwMHN3YTJrb2EyaDZqcGYzNCJ9.wzfF026YmS7lxeAbQOD_tA";
-import * as d3 from 'd3';
-import { onMount } from "svelte";
+  import mapboxgl from "mapbox-gl";
+  import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+  mapboxgl.accessToken = "pk.eyJ1IjoicmFjaGVsbWIiLCJhIjoiY2x1bjFtbDUwMHN3YTJrb2EyaDZqcGYzNCJ9.wzfF026YmS7lxeAbQOD_tA";
+  import * as d3 from 'd3';
+  import { onMount } from "svelte";
+  import { debounce } from "lodash";
 
-let sale = [];
-let permit = [];
-let salesTimeMap;
-let permitTimeMap;
-let mapViewChanged = 0;
-let radiusScale;
-let permitRadiusScale;
-let timeFilter = 2009;
+  let sale = [];
+  let permit = [];
+  let salesTimeMap;
+  let permitTimeMap;
+  let mapViewChanged = 0;
+  let radiusScale;
+  let permitRadiusScale;
+  let timeFilter = 2009;
 
-let legendRadius1 = 0;
-let legendRadius2 = 0;
-let legendRadius3 = 0;
-let legendRadius4 = 0;
-let legendRadius5 = 0;
-let legendRadius6 = 0;
-let legendRadius7 = 0;
-let legendRadius8 = 0;
+  let legendRadius1 = 0;
+  let legendRadius2 = 0;
+  let legendRadius3 = 0;
+  let legendRadius4 = 0;
+  let legendRadius5 = 0;
+  let legendRadius6 = 0;
+  let legendRadius7 = 0;
+  let legendRadius8 = 0;
 
-async function loadMaps() {
-  return Promise.all([
-    new Promise(resolve => {
-      salesTimeMap = new mapboxgl.Map({
-        container: 'salesTimeMap',
-        style: 'mapbox://styles/rachelmb/cluq9tisq01ed01pbebh1c7np',
-        center: [-71.0955, 42.3314],
-        zoom: 11
-      });
+  async function loadMaps() {
+    return Promise.all([
+      new Promise(resolve => {
+        salesTimeMap = new mapboxgl.Map({
+          container: 'salesTimeMap',
+          style: 'mapbox://styles/rachelmb/cluq9tisq01ed01pbebh1c7np',
+          center: [-71.0955, 42.3314],
+          zoom: 11
+        });
 
-      salesTimeMap.on('load', () => resolve(salesTimeMap));
-    }),
-    new Promise(resolve => {
-      permitTimeMap = new mapboxgl.Map({
-        container: 'permitTimeMap',
-        style: 'mapbox://styles/rachelmb/cluisi2c0003o01p2c4mdf1k2',
-        center: [-71.0955, 42.3314],
-        zoom: 11
-      });
+        salesTimeMap.on('load', () => resolve(salesTimeMap));
+      }),
+      new Promise(resolve => {
+        permitTimeMap = new mapboxgl.Map({
+          container: 'permitTimeMap',
+          style: 'mapbox://styles/rachelmb/cluisi2c0003o01p2c4mdf1k2',
+          center: [-71.0955, 42.3314],
+          zoom: 11
+        });
 
-      permitTimeMap.on('load', () => resolve(permitTimeMap));
-    })
-  ]);
-}
+        permitTimeMap.on('load', () => resolve(permitTimeMap));
+      })
+    ]);
+  }
 
-onMount(async () => {
-  [salesTimeMap, permitTimeMap] = await loadMaps();
-  sale = await d3.csv('https://rachelblowes.github.io/Geodata/Boston_sales_2021/salesprices_2010-2022.csv');
-  sale.forEach(d => {
-    d.saleprice = parseFloat(d.saleprice);
+  onMount(async () => {
+    [salesTimeMap, permitTimeMap] = await loadMaps();
+    sale = await d3.csv('https://rachelblowes.github.io/Geodata/Boston_sales_2021/salesprices_2010-2022.csv');
+    sale.forEach(d => {
+      d.saleprice = parseFloat(d.saleprice);
+    });
+    permit = await d3.csv('https://rachelblowes.github.io/Geodata/Boston_sales_2021/permit_valuation_ownertype_2010-2022.csv');
+    permit.forEach(d => {
+      d.valuation = parseFloat(d.valuation);  
+    });
+
+    let maxsaleprice = d3.max(sale, d => d.saleprice);
+    console.log("Max sale price is", maxsaleprice);
+
+    let maxvaluation = d3.max(permit, d => d.valuation);
+    console.log("Max permit valuation is", maxvaluation);
+
+    radiusScale = d3.scaleSqrt()
+      .domain([0, maxsaleprice])
+      .range([1, 30]);
+
+    permitRadiusScale = d3.scaleSqrt()
+      .domain([0, maxvaluation])
+      .range([1, 30]);
+
+    // Batch and debounce map events
+      const debouncedMoveHandler = debounce(() => {
+      mapViewChanged++;
+      const { lng, lat } = salesTimeMap.getCenter();
+      permitTimeMap.setCenter([lng, lat]);
+    }, 50);
+
+    const debouncedZoomHandler = debounce(() => {
+      const zoom = salesTimeMap.getZoom();
+      permitTimeMap.setZoom(zoom);
+    }, 50);
+
+    const debouncedMoveHandlerReverse = debounce(() => {
+      mapViewChanged++;
+      const { lng, lat } = permitTimeMap.getCenter();
+      salesTimeMap.setCenter([lng, lat]);
+    }, 50);
+
+    const debouncedZoomHandlerReverse = debounce(() => {
+      const zoom = permitTimeMap.getZoom();
+      salesTimeMap.setZoom(zoom);
+    }, 50);
+
+    salesTimeMap?.on("move", debouncedMoveHandler);
+    permitTimeMap?.on("move", debouncedMoveHandlerReverse);
+    salesTimeMap?.on("zoom", debouncedZoomHandler);
+    permitTimeMap?.on("zoom", debouncedZoomHandlerReverse);
+
+
+    updateLegendRadius();
   });
-  permit = await d3.csv('https://rachelblowes.github.io/Geodata/Boston_sales_2021/permit_valuation_ownertype_2010-2022.csv');
-  permit.forEach(d => {
-    d.valuation = parseFloat(d.valuation);  
-  });
 
-  let maxsaleprice = d3.max(sale, d => d.saleprice);
-  console.log("Max sale price is", maxsaleprice);
+  function updateLegendRadius() {
+    legendRadius1 = radiusScale(500000);
+    legendRadius2 = radiusScale(5000000);
+    legendRadius3 = radiusScale(50000000);
+    legendRadius4 = radiusScale(332000000);
 
-  let maxvaluation = d3.max(permit, d => d.valuation);
-  console.log("Max permit valuation is", maxvaluation);
+    legendRadius5 = permitRadiusScale(50000);
+    legendRadius6 = permitRadiusScale(500000);
+    legendRadius7 = permitRadiusScale(5000000);
+    legendRadius8 = permitRadiusScale(20000000);
+  }
 
-  radiusScale = d3.scaleSqrt()
-    .domain([0, maxsaleprice])
-    .range([1, 30]);
+  function getCoords(sale) {
+    let point = new mapboxgl.LngLat(+sale.Long, +sale.Lat);
+    let { x, y } = salesTimeMap.project(point);
+    return { cx: x, cy: y };
+  }
 
-  permitRadiusScale = d3.scaleSqrt()
-    .domain([0, maxvaluation])
-    .range([1, 30]);
+  function getPermitCoords(permit) {
+    let point = new mapboxgl.LngLat(+permit.Long, +permit.Lat);
+    let { x, y } = permitTimeMap.project(point);
+    return { cx: x, cy: y };
+  }
 
-  salesTimeMap?.on("move", evt => mapViewChanged++);
-  permitTimeMap?.on("move", evt => mapViewChanged++);
+  export let filteredSales = [];
+  export let filteredPermits = []; 
 
-  updateLegendRadius();
-});
-
-function updateLegendRadius() {
-  legendRadius1 = radiusScale(500000);
-  legendRadius2 = radiusScale(5000000);
-  legendRadius3 = radiusScale(50000000);
-  legendRadius4 = radiusScale(332000000);
-
-  legendRadius5 = permitRadiusScale(50000);
-  legendRadius6 = permitRadiusScale(500000);
-  legendRadius7 = permitRadiusScale(5000000);
-  legendRadius8 = permitRadiusScale(20000000);
-}
-
-function getCoords(sale) {
-  let point = new mapboxgl.LngLat(+sale.Long, +sale.Lat);
-  let { x, y } = salesTimeMap.project(point);
-  return { cx: x, cy: y };
-}
-
-function getPermitCoords(permit) {
-  let point = new mapboxgl.LngLat(+permit.Long, +permit.Lat);
-  let { x, y } = permitTimeMap.project(point);
-  return { cx: x, cy: y };
-   
-}
-
-export let filteredSales = [];
-export let filteredPermits = []; 
-
-$: {
-  filteredSales = timeFilter === 2009 ? [...sale] : sale.filter(sale => {
-    return parseInt(sale.Year) === timeFilter;
-  });
-  filteredPermits = timeFilter === 2009 ? [...permit] : permit.filter(permit => {
-    return parseInt(permit.Year) === timeFilter;
-  });
-}
-
-
+  $: {
+    filteredSales = timeFilter === 2009 ? [...sale] : sale.filter(sale => {
+      return parseInt(sale.Year) === timeFilter;
+    });
+    filteredPermits = timeFilter === 2009 ? [...permit] : permit.filter(permit => {
+      return parseInt(permit.Year) === timeFilter;
+    });
+  }
 </script>
